bc159a9d8b079b3e43d38a0e4f275b756d4b2d45
Samuel Berkun <sberkun@berkeley.edu>
2022-12-29 08:16:35 -0800

    add git portion and make debugging portion more mysterious

--- lab04/src/flik.rs
+++ lab04/src/flik.rs
@@ -1,5 +1,8 @@
-// An Integer tester created by Flik Enterprises.
-// uses cutting-edge match technology
+#[allow(unused_variables)]
+#[allow(unreachable_patterns)]
+
+/// An Integer tester created by Flik Enterprises.
+/// Uses cutting-edge match technology.
 pub fn is_same_number(a: u64, b: u64) -> bool {
     match b {
         a => true,
